931ddfcdd9369df7f676daa7104fad574c4c559b
Abdul Ghaffar Rodriguez <abdul_ghaffar@prominentcode.com>
2022-03-11 14:34:24 -0800

    Actions creators for RESTful routes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,19 @@
-import { SIGN_IN, SIGN_OUT } from './types'
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	EDIT_STREAM,
+	DELETE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+} from "./types";
+import streams from "../apis/streams";
+import { formValues } from "redux-form";
 
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
-        payload: userId
+		payload: userId,
 	};
 };
 
@@ -12,3 +22,28 @@ export const signOut = () => {
 		type: SIGN_OUT,
 	};
 };
+
+export const createStream = (formValues) => async (dispatch) => {
+	const response = await streams.post("/streams", formValues);
+	dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+	const response = await streams.get("/streams");
+	dispatch({ type: FETCH_STREAMS, payload: response.data})
+}
+
+export const fetchStream = (id) => async (dispatch) => {
+	const response = await streams.get(`/streams/${id}`);
+	dispatch({ type: FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, formValues) => (dispatch) => {
+	const response = streams.put(`/streams/${id}`, formValues);
+	dispatch({ type: EDIT_STREAM, payload: response.data })
+}
+
+export const deleteStream = (id) => async (dispatch) => {
+	await streams.delete(`/streams/${id}`);
+	dispatch({ typeof: DELETE_STREAM, payload: id })
+}
