bf5f8d9e77c125cb437e51e6047d0da537ae0588
Abdul Ghaffar Rodriguez <abdul_ghaffar@prominentcode.com>
2022-03-12 15:12:40 -0800

    build stream reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import AuthReducer from "./AuthReducer";
+import StreamReducer from "./StreamReducer";
 
 export default combineReducers({
     auth: AuthReducer,
-    form: formReducer
+    form: formReducer,
+    streams: StreamReducer
 })
